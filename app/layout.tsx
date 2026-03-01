eafaa45ce03950b1a27a35ee77b56a1c2e9ce319
Pat <schwarzopat@gmail.com>
2026-03-01 19:48:32 +1100

    fix pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${grotesk.variable}`}>
+      <head>
+        <title>Silent Axis</title>
+      </head>
       <body>{children}</body>
     </html>
   );
